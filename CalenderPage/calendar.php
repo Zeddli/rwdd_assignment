1f6a21a907d080978e90fef373e3f54aa131b8cd
Zeddli <ruj8320@gmail.com>
2025-09-03 19:33:40 +0800

    import navbar to calender

---
+++ CalenderPage/calendar.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Search</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="../Navbar/base.css">
+    <link rel="stylesheet" href="../Navbar/navbar.css">
+    <link rel="stylesheet" href="calendar.css">
+
+</head>
+<body>
+    <!-- Main Navigation Sidebar Container -->
+    <?php include "../Navbar/navbar.php"; ?>
+
+    <div class="main-content">
+        <h1>Calendar</h1>
+        <div class="calendar-container">
+            <!-- Calendar will be rendered here -->
+             <h1>Calendar page</h1>
+        </div>
+    </div>
+
+    <!-- JS modules for sidebar -->
+    <script src="../Navbar/core.js"></script>
+    <script src="../Navbar/dropdowns.js"></script>
+    <script src="../Navbar/editing.js"></script>
+    <script src="../Navbar/workspaces.js"></script>
+    <script src="../Navbar/tasks.js"></script>
+    <script src="../Navbar/sidebar.js"></script>
+    <script src="../Navbar/main.js"></script>
+</body>
+</html>
