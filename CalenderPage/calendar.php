19c28c85896a7494b8ec97ee6b376ebd5c97f680
lahalaha <wongkamfatt9@gmail.com>
2025-10-24 20:43:28 +0800

    update calender page icon by include Head.php

--- CalenderPage/calendar.php
+++ CalenderPage/calendar.php
@@ -1,18 +1,27 @@
 <!-- Calendar main view with To-Do Sidebar -->
 <?php
-session_start();
-// Check if user is logged in
-if (!isset($_SESSION['userInfo']['userID'])) {
-    header("Location: ../LoginSignup/landing/login/login.php");
-    exit();
-}
+// session_start();
+// // Check if user is logged in
+// if (!isset($_SESSION['userInfo']['userID'])) {
+//     header("Location: ../LoginSignup/landing/login/login.php");
+//     exit();
+// }
+    include "../Head/Head.php";
+
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
+    // $_SESSION["workspaceID"] = 1; //CHANGEEEEEEEEEEEEE!!!!!!!!!!!!
+    if(!isset($_SESSION["workspaceID"])){
+        header("Location: ../HomePage/Home.php");
+    }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Calendar - ProTask</title>
+    <!-- <title>Calendar - ProTask</title> -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <link rel="stylesheet" href="../Navbar/styles/base.css">
